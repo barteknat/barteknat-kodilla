0796e242d3db524224eededa9ff3cd3a5960763a
barteknat <73423811+barteknat@users.noreply.github.com>
2021-01-31 19:25:51 +0100

    KOD-6.2: added Calculator class and tested its methods (#2)
    Co-authored-by: Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,20 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Module 6. Introduction to software testing.");
+
+        if (new Calculator().add(7, 10) == 17 && new Calculator().add(10, 7) == 17) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        if (new Calculator().subtract(7, 10) == -3 && new Calculator().subtract(10, 7) == 3) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
