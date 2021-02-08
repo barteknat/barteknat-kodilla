dce3f10fed6770c4491289d6dd2ac7b60c6267e1
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-02-08 23:01:04 +0100

    KOD-8.3 rev.1: taken out flights map from findFlight method and corrected validation for throwing RouteNotFoundException

--- kodilla-exception/src/main/java/com/kodilla/exception/test3/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test3/FlightSearch.java
@@ -5,18 +5,34 @@ import java.util.Map;
 
 public class FlightSearch {
 
-    public void findFlight(Flight flight) throws RouteNotFoundException {
-        Map<String, Boolean> flights = new HashMap<>();
+    private final Map<String, Boolean> flights = new HashMap<>();
+
+    public FlightSearch() {
         flights.put("Rome", true);
+        flights.put("Madrid", true);
+        flights.put("London", false);
+        flights.put("Berlin", true);
+        flights.put("Paris", false);
+        flights.put("Antwerp", false);
+        flights.put("Lisbon", true);
+    }
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        String arrivalAirport = "";
+        boolean airportOpen = true;
         if (flight != null) {
             for (Map.Entry<String, Boolean> entry : flights.entrySet()) {
-                if (!entry.getKey().equals(flight.getArrivalAirport())) {
-                    throw new RouteNotFoundException("No arrival airport.");
-                } else if (entry.getKey().equals(flight.getArrivalAirport()) && entry.getValue() == false) {
-                    System.out.println("The airport is temporary closed.");
-                } else {
-                    System.out.println("The flight is available.");
+                if (entry.getKey().equals(flight.getArrivalAirport())) {
+                    arrivalAirport = entry.getKey();
+                    airportOpen = entry.getValue();
                 }
+            }
+            if (airportOpen == false) {
+                System.out.println("Airport is temporary closed");
+            } else if (arrivalAirport.equals(flight.getArrivalAirport())) {
+                System.out.println("Flight is available");
+            } else {
+                throw new RouteNotFoundException("No arrival airport");
             }
         } else {
             throw new RouteNotFoundException("Object flight was null.");
