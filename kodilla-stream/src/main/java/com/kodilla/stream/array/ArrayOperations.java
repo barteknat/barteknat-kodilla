d3bb5db4796ff83af73bfd83e53ea82ac1e0ef10
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-02-09 19:53:51 +0100

    KOD-7.5 rev.2: improved validation in ArrayOperations class

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -5,18 +5,16 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
-        double average = 0;
-        if (numbers != null) {
-            if (numbers.length > 0) {
-                int[] finalNumbers = numbers;
-                IntStream.range(0, finalNumbers.length)
-                        .map(n -> finalNumbers[n])
-                        .forEach(System.out::println);
-                average = IntStream.range(0, finalNumbers.length)
-                        .map(n -> finalNumbers[n])
-                        .average().getAsDouble();
-            }
-        }
+        if (numbers == null) return 0;
+        int[] finalNumbers = numbers;
+        IntStream.range(0, finalNumbers.length)
+                .map(n -> finalNumbers[n])
+                .forEach(System.out::println);
+
+        double average = IntStream.range(0, finalNumbers.length)
+                .map(n -> finalNumbers[n])
+                .average()
+                .orElse(0);
         return average;
     }
 }
