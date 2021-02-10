9debe70076aef7b902df7c536ed3a66b52e6b6cb
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-02-10 21:31:34 +0100

    MOD-7.6 rev.1: moved private method to the end of the class, added delta in assertion, used Chronounit for counting days and added orElse to avoid NPE in stream

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -1,53 +1,15 @@
 package com.kodilla.stream.portfolio;
 
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
-
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTestSuite {
-
-    private Board prepareTestData() {
-        //users
-        User user1 = new User("developer1", "John Smith");
-        User user2 = new User("projectmanager1", "Nina White");
-        User user3 = new User("developer2", "Emilia Stephanson");
-        User user4 = new User("developer3", "Konrad Bridge");
-
-        //tasks
-        Task task1 = new Task("Microservice for taking temperatures", "Write and test the microservice taking the temperature from external service", user1, user2, LocalDate.now().minusDays(20), LocalDate.now().plusDays(30));
-        Task task2 = new Task("HQLs for analysis", "Prepare some HQL queries for analysis", user1, user2, LocalDate.now().minusDays(20), LocalDate.now().minusDays(5));
-        Task task3 = new Task("Temperatures entity", "Prepare entity for temperatures", user3, user2, LocalDate.now().minusDays(20), LocalDate.now().plusDays(15));
-        Task task4 = new Task("Own logger", "Refactor company logger to meet our needs", user3, user2, LocalDate.now().minusDays(10), LocalDate.now().plusDays(25));
-        Task task5 = new Task("Optimize searching", "Archive data searching has to be optimized", user4, user2, LocalDate.now(), LocalDate.now().plusDays(5));
-        Task task6 = new Task("Use Streams", "use Streams rather than for-loops in predictions", user4, user2, LocalDate.now().minusDays(15), LocalDate.now().minusDays(2));
-
-        //taskLists
-        TaskList taskListToDo = new TaskList("To do");
-        taskListToDo.addTask(task1);
-        taskListToDo.addTask(task3);
-
-        TaskList taskListInProgress = new TaskList("In progress");
-        taskListInProgress.addTask(task5);
-        taskListInProgress.addTask(task4);
-        taskListInProgress.addTask(task2);
-
-        TaskList taskListDone = new TaskList("Done");
-        taskListDone.addTask(task6);
-
-        //board
-        Board project = new Board("Project Weather Prediction");
-        project.addTaskList(taskListToDo);
-        project.addTaskList(taskListInProgress);
-        project.addTaskList(taskListDone);
-
-        return project;
-    }
 
     @Test
     void testAddTaskList() {
@@ -71,8 +33,6 @@ public class BoardTestSuite {
                 .flatMap(l -> l.getTasks().stream())
                 .filter(t -> t.getAssignedUser().equals(user))
                 .collect(toList());
-
-
 
         //Then
         assertEquals(2, tasks.size());
@@ -118,32 +78,7 @@ public class BoardTestSuite {
     }
 
     @Test
-    void testAddTaskListAverageWorkingOnTaskMethod1() {
-        //Given
-        Board project = prepareTestData();
-
-        //When
-        List<TaskList> inProgressTasks = new ArrayList<>();
-        inProgressTasks.add(new TaskList("In progress"));
-        Long sumOfTasks = project.getTaskLists().stream()
-                .filter(inProgressTasks::contains)
-                .flatMap(tl -> tl.getTasks().stream())
-                .count();
-        Long sumOfDaysFromCreatedToNow = project.getTaskLists().stream()
-                .filter(inProgressTasks::contains)
-                .flatMap(tl -> tl.getTasks().stream())
-                .map(t -> LocalDate.now().toEpochDay() - t.getCreated().toEpochDay())
-                .reduce(0L, (sum, current) -> sum = sum + current);
-        double averageWorkingOnTaskMethod1 = (double) sumOfDaysFromCreatedToNow/sumOfTasks;
-
-        //Then
-        assertEquals(3, sumOfTasks);
-        assertEquals(30, sumOfDaysFromCreatedToNow);
-        assertEquals(10, averageWorkingOnTaskMethod1);
-    }
-
-    @Test
-    void testAddTaskListAverageWorkingOnTaskMethod2() {
+    void testAddTaskListAverageWorkingOnTask() {
         //Given
         Board project = prepareTestData();
 
@@ -153,13 +88,51 @@ public class BoardTestSuite {
         List<Long> daysList = project.getTaskLists().stream()
                 .filter(inProgressTasks::contains)
                 .flatMap(tl -> tl.getTasks().stream())
-                .map(t -> LocalDate.now().toEpochDay() - t.getCreated().toEpochDay())
+                .map(t -> ChronoUnit.DAYS.between(t.getCreated(), LocalDate.now()))
                 .collect(toList());
-        double averageWorkingOnTaskMethod2 = IntStream.range(0, daysList.size())
+        double averageWorkingOnTask = IntStream.range(0, daysList.size())
                 .map(n -> Math.toIntExact(daysList.get(n)))
-                .average().getAsDouble();
+                .average()
+                .orElse(0);
 
         //Then
-        assertEquals(10, averageWorkingOnTaskMethod2);
+        assertEquals(10, averageWorkingOnTask, 0.1);
+    }
+
+    private Board prepareTestData() {
+        //users
+        User user1 = new User("developer1", "John Smith");
+        User user2 = new User("projectmanager1", "Nina White");
+        User user3 = new User("developer2", "Emilia Stephanson");
+        User user4 = new User("developer3", "Konrad Bridge");
+
+        //tasks
+        Task task1 = new Task("Microservice for taking temperatures", "Write and test the microservice taking the temperature from external service", user1, user2, LocalDate.now().minusDays(20), LocalDate.now().plusDays(30));
+        Task task2 = new Task("HQLs for analysis", "Prepare some HQL queries for analysis", user1, user2, LocalDate.now().minusDays(20), LocalDate.now().minusDays(5));
+        Task task3 = new Task("Temperatures entity", "Prepare entity for temperatures", user3, user2, LocalDate.now().minusDays(20), LocalDate.now().plusDays(15));
+        Task task4 = new Task("Own logger", "Refactor company logger to meet our needs", user3, user2, LocalDate.now().minusDays(10), LocalDate.now().plusDays(25));
+        Task task5 = new Task("Optimize searching", "Archive data searching has to be optimized", user4, user2, LocalDate.now(), LocalDate.now().plusDays(5));
+        Task task6 = new Task("Use Streams", "use Streams rather than for-loops in predictions", user4, user2, LocalDate.now().minusDays(15), LocalDate.now().minusDays(2));
+
+        //taskLists
+        TaskList taskListToDo = new TaskList("To do");
+        taskListToDo.addTask(task1);
+        taskListToDo.addTask(task3);
+
+        TaskList taskListInProgress = new TaskList("In progress");
+        taskListInProgress.addTask(task5);
+        taskListInProgress.addTask(task4);
+        taskListInProgress.addTask(task2);
+
+        TaskList taskListDone = new TaskList("Done");
+        taskListDone.addTask(task6);
+
+        //board
+        Board project = new Board("Project Weather Prediction");
+        project.addTaskList(taskListToDo);
+        project.addTaskList(taskListInProgress);
+        project.addTaskList(taskListDone);
+
+        return project;
     }
 }
