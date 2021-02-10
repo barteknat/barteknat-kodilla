e03c6184c9d4352ceaefd48e02f072a9965a53be
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-02-10 20:37:34 +0100

    KOD-7.4 rev.2: data for constructor in World class prepared in WorldTestSuite

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -1,34 +1,38 @@
 package com.kodilla.stream.world;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class World {
 
-    private List<Continent> continents = new ArrayList<>();
+    private List<Continent> continents;
 
-    public void addContinents() {
-        continents = Arrays.asList(
-                new Continent("Europe", Arrays.asList(
-                        new Country("Poland", new BigDecimal("40000000")),
-                        new Country("France", new BigDecimal("80000000")),
-                        new Country("Germany", new BigDecimal("90000000")))),
-                new Continent("Asia", Arrays.asList(
-                        new Country("Russia", new BigDecimal("130000000")),
-                        new Country("China", new BigDecimal("1300000000")),
-                        new Country("India", new BigDecimal("1000000000")))),
-                new Continent("North America", Arrays.asList(
-                        new Country("Canada", new BigDecimal("120000000")),
-                        new Country("United States", new BigDecimal("230000000")),
-                        new Country("Mexico", new BigDecimal("150000000")))));
+    public World(List<Continent> continents) {
+        this.continents = continents;
+    }
+
+    public List<Continent> getContinents() {
+        return continents;
     }
 
     public BigDecimal getPeopleQuantity() {
-        return continents.stream()
+        return getContinents().stream()
                 .flatMap(continent -> continent.getCountries().stream())
                 .map(country -> country.getPeopleQuantity())
                 .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        World world = (World) o;
+        return Objects.equals(continents, world.continents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(continents);
     }
 }
