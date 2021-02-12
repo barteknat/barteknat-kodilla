ca9e9967a893dd917aeabdf5aa9b77df1be24782
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-02-12 20:54:00 +0100

    MOD-7.6 rev.2: corrected implementation of given/when/then section

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -26,9 +26,9 @@ public class BoardTestSuite {
     void testAddTaskListFindUsersTasks() {
         //Given
         Board project = prepareTestData();
+        User user = new User("developer1", "John Smith");
 
         //When
-        User user = new User("developer1", "John Smith");
         List<Task> tasks = project.getTaskLists().stream()
                 .flatMap(l -> l.getTasks().stream())
                 .filter(t -> t.getAssignedUser().equals(user))
@@ -44,11 +44,11 @@ public class BoardTestSuite {
     void testAddTaskListFindOutdatedTasks() {
         //Given
         Board project = prepareTestData();
-
-        //When
         List<TaskList> undoneTasks = new ArrayList<>();
         undoneTasks.add(new TaskList("To do"));
         undoneTasks.add(new TaskList("In progress"));
+
+        //When
         List<Task> tasks = project.getTaskLists().stream()
                 .filter(undoneTasks::contains)
                 .flatMap(tl -> tl.getTasks().stream())
@@ -64,10 +64,10 @@ public class BoardTestSuite {
     void testAddTaskListFindLongTasks() {
         //Given
         Board project = prepareTestData();
-
-        //When
         List<TaskList> inProgressTasks = new ArrayList<>();
         inProgressTasks.add(new TaskList("In progress"));
+
+        //When
         List<Task> tasks = project.getTaskLists().stream()
                 .filter(inProgressTasks::contains)
                 .flatMap(tl -> tl.getTasks().stream())
@@ -81,8 +81,6 @@ public class BoardTestSuite {
     void testAddTaskListAverageWorkingOnTask() {
         //Given
         Board project = prepareTestData();
-
-        //When
         List<TaskList> inProgressTasks = new ArrayList<>();
         inProgressTasks.add(new TaskList("In progress"));
         List<Long> daysList = project.getTaskLists().stream()
@@ -90,6 +88,8 @@ public class BoardTestSuite {
                 .flatMap(tl -> tl.getTasks().stream())
                 .map(t -> ChronoUnit.DAYS.between(t.getCreated(), LocalDate.now()))
                 .collect(toList());
+
+        //When
         double averageWorkingOnTask = IntStream.range(0, daysList.size())
                 .map(n -> Math.toIntExact(daysList.get(n)))
                 .average()
@@ -105,7 +105,6 @@ public class BoardTestSuite {
         User user2 = new User("projectmanager1", "Nina White");
         User user3 = new User("developer2", "Emilia Stephanson");
         User user4 = new User("developer3", "Konrad Bridge");
-
         //tasks
         Task task1 = new Task("Microservice for taking temperatures", "Write and test the microservice taking the temperature from external service", user1, user2, LocalDate.now().minusDays(20), LocalDate.now().plusDays(30));
         Task task2 = new Task("HQLs for analysis", "Prepare some HQL queries for analysis", user1, user2, LocalDate.now().minusDays(20), LocalDate.now().minusDays(5));
@@ -113,20 +112,16 @@ public class BoardTestSuite {
         Task task4 = new Task("Own logger", "Refactor company logger to meet our needs", user3, user2, LocalDate.now().minusDays(10), LocalDate.now().plusDays(25));
         Task task5 = new Task("Optimize searching", "Archive data searching has to be optimized", user4, user2, LocalDate.now(), LocalDate.now().plusDays(5));
         Task task6 = new Task("Use Streams", "use Streams rather than for-loops in predictions", user4, user2, LocalDate.now().minusDays(15), LocalDate.now().minusDays(2));
-
         //taskLists
         TaskList taskListToDo = new TaskList("To do");
         taskListToDo.addTask(task1);
         taskListToDo.addTask(task3);
-
         TaskList taskListInProgress = new TaskList("In progress");
         taskListInProgress.addTask(task5);
         taskListInProgress.addTask(task4);
         taskListInProgress.addTask(task2);
-
         TaskList taskListDone = new TaskList("Done");
         taskListDone.addTask(task6);
-
         //board
         Board project = new Board("Project Weather Prediction");
         project.addTaskList(taskListToDo);
