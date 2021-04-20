1c8c9cddca87b505dc456518f4adb49d052d23d1
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-04-20 18:01:38 +0200

    KOD-14.2_rev.2: corrected Calculator class by changed methods to return type

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -9,21 +9,29 @@ public class Calculator {
     @Autowired
     private Display display;
 
-    public void add(double a, double b) {
-        display.displayValue(a + b);
+    public double add(double a, double b) {
+        double result = a + b;
+        display.displayValue(result);
+        return result;
     }
 
-    public void sub(double a, double b) {
-        display.displayValue(a - b);
+    public double sub(double a, double b) {
+        double result = a - b;
+        display.displayValue(result);
+        return result;
     }
 
-    public void mul(double a, double b) {
-        display.displayValue(a * b);
+    public double mul(double a, double b) {
+        double result = a * b;
+        display.displayValue(result);
+        return result;
     }
 
-    public void div(double a, double b) {
-        if (b == 0) return;
-        display.displayValue(a / b);
+    public double div(double a, double b) {
+        if (b == 0) return 0;
+        double result = a / b;
+        display.displayValue(result);
+        return result;
     }
 }
 
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -2,6 +2,7 @@ package com.kodilla.spring.calculator;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,32 +17,45 @@ public class CalculatorTestSuite {
     void testAdd() {
         //Given
 
-        //When & Then
-        calculator.add(5.5, 4.5);
+        //When
+        double score = calculator.add(5.5, 4.5);
+
+        //Then
+        assertEquals(5.5 + 4.5, score);
     }
 
     @Test
     void testSub() {
         //Given
 
-        //When & Then
-        calculator.sub(5.5, 4.5);
+        //When
+        double score = calculator.sub(5.5, 4.5);
+
+        //Then
+        assertEquals(5.5 - 4.5, score);
     }
 
     @Test
     void testMul() {
         //Given
 
-        //When & Then
-        calculator.mul(2.0, 4.5);
+        //When
+        double score = calculator.mul(2.0, 4.5);
+
+        //Then
+        assertEquals(2.0 * 4.5, score);
     }
 
     @Test
     void testDiv() {
         //Given
 
-        //When & Then
-        calculator.div(3, 2);
+        //When
+
+        double score = calculator.div(3, 2);
+
+        //Then
+        assertEquals(3.0 / 2, score);
     }
 }
 
